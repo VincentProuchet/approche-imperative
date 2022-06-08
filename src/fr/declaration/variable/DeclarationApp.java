e1d2e635c1a8cfdb6f5604955ae3849fb739c97c
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-06-08 15:52:04 +0200

    TP 03 ajout d'un test de variable avec une conversion oltale implicite.

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -15,22 +15,24 @@ public class DeclarationApp {
 		double grosseVirgule = 12316.546879d;
 		char lettre = 'C';
 		boolean binaire = true;
+		int g = 057;
 		String phrase = "Une phrase d'introduction";
 		String randomString = "Voici le r�sultat d'un calcul :\n 1+5 =6";
 		
-
-	
-		System.out.print( "un byte  : " + octal  +"\n");
-		System.out.print( "un short : " + court  +"\n");
-		System.out.print( "un int  : " + entier +"\n");
-		System.out.print( "un long  : " + tooLoooong  +"\n");
-		System.out.print( "un float  : " + virgule +"\n");
-		System.out.print( "un double  : " + grosseVirgule +"\n");
-		System.out.print( "un char  : " + lettre +"\n");
-		System.out.print( "un boolean : " + binaire +"\n");
-		System.out.print( "un String   : " + phrase +"\n");
+		System.out.println("un byte  : " + octal);	
+	 // raccourci syso + CTRL + SPACE
+		System.out.println( "un short    : " + court  );
+		System.out.println( "un int      : " + entier );
+		System.out.println( "un long     : " + tooLoooong  );
+		System.out.println( "un float    : " + virgule );
+		System.out.println( "un double   : " + grosseVirgule );
+		System.out.println( "un char     : " + lettre );
+		System.out.println( "un boolean  : " + binaire );
+		System.out.println( "un String   : " + phrase );
+		System.out.println("un octal     :"  + g);
 		
 		System.out.print( randomString +"\n" );
+		
 	}
 
 }
