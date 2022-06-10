0bc39da503e8149632decfc2fef6c7121a55492d
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-06-10 18:53:27 +0200

    Complété le jeu des 21 Baton en impératif Je compte bien me le faire façon objet histoire de ré-équilibrer mon Karma

--- src/fr/algorithmie/Interactif21Baton.java
+++ src/fr/algorithmie/Interactif21Baton.java
@@ -1,5 +1,6 @@
 package fr.algorithmie;
 
+import java.util.Random;
 import java.util.Scanner;
 /*
  * Le jeu est simple mais la réalisation est plus délicate. Vous allez jouer contre l’ordinateur.
@@ -10,46 +11,72 @@ import java.util.Scanner;
 
 public class Interactif21Baton {
 
-	Scanner scanner = new Scanner(System.in);
 	static int batonsAuDepart = 21;
-	int batons = batonsAuDepart;
-	int nbEC = 0;
 
 	public static void main(String[] args) {
-			
+		Scanner scanner = new Scanner(System.in);
 		Interactif21Baton jeux = new Interactif21Baton();
+		boolean continuer = true;
+		int nbEC = 0;
 		
-
 		int joueur1 = 0;
 		int joueur2 = 0;
-		jeux.batons = batonsAuDepart;
+		int gagnant = 0;
+		int batons = Interactif21Baton.batonsAuDepart;
+		Random random = new Random();
+		
+		
 		System.out.println(" Les 21 Batons  \n" + "	Il y a 21 batons sur la table. \n"
 				+ "	Vous et votre adversaire pouver en prendre \n" + "	de 1 à 3 batons. \n"
 				+ " Celui qui prend le dernier baton perd la partie. \n");
+
+		do {
+			batons = Interactif21Baton.prenezDesBatons(1, joueur1, scanner, batons);
+			if(batons>0) {
+				// random
+				batons -= random.nextInt(1,3);			
+				// affichage du nombre de baton restantF
+				System.out.println("Il reste " + batons);
+				// human input
+				//Interactif21Baton.prenezDesBatons(2, joueur2, scanner, batons);
+						
+				continuer = true;
+				if(batons>0) {
+					continuer = true;
+				}
+				else {
+					continuer = false;
+					gagnant = 1;
+				}
+			}else {
+				continuer = false;
+				gagnant = 2;
+			}
+			
+			
+		} while (continuer);
+		System.out.println("Le joueur "+ gagnant +" remporte la partie" );
 		
-		jeux.prenezDesBatons(1,joueur1);
-		jeux.prenezDesBatons(2, joueur2);
-		
-		
-		
+
 	}
 
-	public void prenezDesBatons( int idJoueur ,int joueur) {
+	public static int prenezDesBatons(int idJoueur, int joueur, Scanner scanner, int batons) {
 
 		boolean again = true;
-		String texte ="- Joueur " + idJoueur + ", prenez de un à 3 batons ";
-		
+		String texte = "- Joueur " + idJoueur + ", prenez de un à 3 batons ";
 
 		do {
 			System.out.println(texte);
-			this.nbEC = scanner.nextInt();
+			int nbEC = scanner.nextInt();
 			if (nbEC == 1 || nbEC == 2 || nbEC == 3) {
 				// cas d'entrée dans la marge autorisé
-				if( (this.batons -= this.nbEC) <=0 ) {
-					
+				// test pour voir si il reste assez de batons
+				if ((batons -= nbEC) <= 0) {
+					batons -= nbEC;
+					joueur += nbEC;
 				}
-				this.batons -= this.nbEC;
-				joueur += this.nbEC;
+				
+				
 				// logique de prise de baton
 				again = false;
 
@@ -58,8 +85,11 @@ public class Interactif21Baton {
 				texte = " vous ne pouvez pas prendre plus de 3 baton par tours ";
 			}
 		} while (again);
-		
-		texte = "Il reste " + this.batons; 
+
+		texte = "Il reste " + batons;
 		// affichage du nombre de baton restantF
+		System.out.println(texte);
+
+		return batons;
 	}
 }
