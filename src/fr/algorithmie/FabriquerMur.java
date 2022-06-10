98eb40eb8810f659ab0c75cc0e476314b662fb3a
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-06-11 01:05:28 +0200

    Terminé fabriquer Mur en approche impérative
    Maintenant je peux aller me coucher

--- src/fr/algorithmie/FabriquerMur.java
+++ src/fr/algorithmie/FabriquerMur.java
@@ -8,48 +8,54 @@ package fr.algorithmie;
 public class FabriquerMur {
 // TODO classe à terminer
 	public static void main(String[] args) {
-		
+
 		// Tests de vérification
-		verifier(3, 1, 8, true);
-		verifier(3, 1, 9, false);
-		verifier(3, 2, 10, true);
-		verifier(3, 2, 8, true);
-		verifier(3, 2, 9, false);
-		verifier(6, 1, 11, true);
+		verifier(3, 1, 	8, 	true);
+		verifier(3, 1,	9, false);
+		verifier(3, 2, 10, 	true);
+		verifier(3, 2,	8, 	true);
+		verifier(3, 2, 	9, false);
+		verifier(6, 1, 11, 	true);
 		verifier(6, 0, 11, false);
-		verifier(1, 4, 11, true);
-		verifier(0, 3, 10, true);
+		verifier(1, 4, 11,	true);
+		verifier(0, 3, 10, 	true);
 		verifier(1, 4, 12, false);
-		verifier(3, 1, 7, true);
-		verifier(1, 1, 7, false);
+		verifier(3, 1, 	7, 	true);
+		verifier(1, 1, 	7, false);
 	}
 
-	public static boolean fabriquerMur(int nbSmal, int nbBig, int longueur) {
+	public static boolean fabriquerMur(int nbSmall, int nbBig, int longueur) {
+		// tant que le mur est pas fait
 		boolean resultat = false;
-		int longueurRestante = longueur;
-		int bigNeeded = 0;
-		int smallNeeded = 0;
+		/// ça c'est mon TOC
 		int bigSize = 5;
 		int smallSize = 1;
+		///
 		// Combien de grandes briques entières sont nécessaires
-		bigNeeded =  (longueur/bigSize);
-		// logique de controle pour ne pas en retirer plsu que le stock
-		
-		// logique de retrait
-		
-		// combien de petite brique sont neccessaires
-		
-		// logique de controle pour ne pas en retirer plsu que le stock
-		
-		// logique de retrait
-				
-		if(longueurRestante == 0 ) {
+		longueur = RetirerBrique(bigSize, nbBig, longueur);
+		longueur = RetirerBrique(smallSize, nbSmall, longueur);
+		// puis on controle l'état de la longueur restante
+		if (longueur <= 0) {
 			resultat = true;
 		}
-	
-		
 
 		return resultat;
+	}
+	private static int RetirerBrique(int size, int briques, int longueur ) {
+		// on calcule la quantité nécessaire 
+		int quantite = (int) longueur / size;
+		// logique de controle pour limiter le stock
+		// si on à plus de brique que necessaire
+		if (quantite <= briques) {
+			// on pose tout ce qu'on peut
+			longueur -= quantite * size;
+		}
+		// si on a pas suffisement de briques 
+		else {
+			// on pose tout le stock
+			longueur -= (int) size * briques;
+		}
+		return longueur;
 	}
 
 	private static void verifier(int nbSmall, int nbBig, int longueur, boolean b) {
